333d43993b4c6437ac7812a9171aec11e77564e5
edmundo papi <edmundopierre@yahoo.com>
2020-08-26 09:40:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@
 class CreateCostumeStores < Migration[5.2]
 
 def change
-  create_table :costumestores do |t|
+  create_table :costume_stores do |t|
     t.string :name
     t.string :location
     t.integer :costume_inventory
