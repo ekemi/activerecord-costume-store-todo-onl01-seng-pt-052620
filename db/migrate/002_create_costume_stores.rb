a2fc0c91662e9acf84c56507209f9923e5f2525c
edmundo papi <edmundopierre@yahoo.com>
2020-08-26 09:44:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,8 @@ def change
     t.integer :costume_inventory
     t.integer :num_of_employees
     t.boolean :still_in_business
-    t.timestamps
+    t.datetime :opening_time
+    t.datetime :closing_time
   end
 end
 end
